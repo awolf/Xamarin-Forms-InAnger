6afa036abbdde37774d71e4eb27944f7ccc37993
Adam Wolf <adam@adamjwolf.com>
2015-04-23 15:00:26 -0700

    Add bama pictures to droid

--- Droid/Resources/Resource.designer.cs
+++ Droid/Resources/Resource.designer.cs
@@ -47,28 +47,55 @@ namespace InAnger.Droid
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int chat = 2130837504;
+			public const int bama1 = 2130837504;
 			
 			// aapt resource value: 0x7f020001
-			public const int dome = 2130837505;
+			public const int bama2 = 2130837505;
 			
 			// aapt resource value: 0x7f020002
-			public const int face = 2130837506;
+			public const int bama3 = 2130837506;
 			
 			// aapt resource value: 0x7f020003
-			public const int icon = 2130837507;
+			public const int bama4 = 2130837507;
 			
 			// aapt resource value: 0x7f020004
-			public const int pin = 2130837508;
+			public const int bama5 = 2130837508;
 			
 			// aapt resource value: 0x7f020005
-			public const int pindrop = 2130837509;
+			public const int bama6 = 2130837509;
 			
 			// aapt resource value: 0x7f020006
-			public const int settings = 2130837510;
+			public const int bama7 = 2130837510;
 			
 			// aapt resource value: 0x7f020007
-			public const int southmountain = 2130837511;
+			public const int bama8 = 2130837511;
+			
+			// aapt resource value: 0x7f020008
+			public const int bama9 = 2130837512;
+			
+			// aapt resource value: 0x7f020009
+			public const int chat = 2130837513;
+			
+			// aapt resource value: 0x7f02000a
+			public const int dome = 2130837514;
+			
+			// aapt resource value: 0x7f02000b
+			public const int face = 2130837515;
+			
+			// aapt resource value: 0x7f02000c
+			public const int icon = 2130837516;
+			
+			// aapt resource value: 0x7f02000d
+			public const int pin = 2130837517;
+			
+			// aapt resource value: 0x7f02000e
+			public const int pindrop = 2130837518;
+			
+			// aapt resource value: 0x7f02000f
+			public const int settings = 2130837519;
+			
+			// aapt resource value: 0x7f020010
+			public const int southmountain = 2130837520;
 			
 			static Drawable()
 			{
